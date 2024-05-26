c2e8421f5ce8a8631f17ceff9456a1dc9733c36a
Sergiy Horodetskiy <bryklin.best.1994@gmail.com>
2024-05-26 17:13:42 +0300

    data quiz

--- src/assets/data.js
+++ src/assets/data.js
@@ -211,92 +211,108 @@ export const arrQuiz = [
         title: 'Quiz Angular',
         questions: [
             {
-                // id: 'id-1',
-
-                titleQuestions: 'Which company created React.js?',
+                titleQuestions: 'Що таке Angular?',
                 answerOption: [
-                    { answerText: 'A. Google', isCorrect: false },
-                    { answerText: 'B. Facebook', isCorrect: true },
-                    { answerText: 'C. Microsoft', isCorrect: false },
-                    { answerText: 'D. Twitter', isCorrect: false },
+                    {
+                        answerText: 'JavaScript library for building interfaces',
+                        isCorrect: true,
+                    },
+                    { answerText: 'CSS framework', isCorrect: false },
+                    { answerText: 'HTML preprocessor', isCorrect: false },
+                    { answerText: 'DBMS', isCorrect: false },
                 ],
             },
             {
-                // id: 'id-2',
-
-                titleQuestions: 'What is JSX in React?',
+                titleQuestions: 'Who developed Angular?',
                 answerOption: [
-                    { answerText: 'JavaScript XML', isCorrect: true },
-                    { answerText: 'JavaScript Extension', isCorrect: false },
-                    { answerText: 'JavaScript eXample', isCorrect: false },
-                    { answerText: 'JavaScript eXperience', isCorrect: false },
+                    { answerText: 'Google', isCorrect: true },
+                    { answerText: 'Facebook', isCorrect: false },
+                    { answerText: 'Microsoft', isCorrect: false },
+                    { answerText: 'Twitter', isCorrect: false },
                 ],
             },
             {
-                // id: 'id-3',
-
-                titleQuestions: 'Which command is used to create a new React project?',
+                titleQuestions: 'What is Angular CLI?',
                 answerOption: [
-                    { answerText: 'npx create-react-app', isCorrect: true },
-                    { answerText: 'npm init react-app', isCorrect: false },
-                    { answerText: 'npm create-react-app', isCorrect: false },
-                    { answerText: 'npx init react-app', isCorrect: false },
+                    { answerText: 'Command line interface for Angular', isCorrect: true },
+                    { answerText: 'A library for state management', isCorrect: false },
+                    { answerText: 'A tool for testing', isCorrect: false },
+                    { answerText: 'Interface for customizing styles', isCorrect: false },
                 ],
             },
             {
-                // id: 'id-4',
-
-                titleQuestions: 'What is a component in React?',
+                titleQuestions: 'What is a Component in Angular?',
                 answerOption: [
-                    { answerText: 'A function or class that returns HTML', isCorrect: true },
-                    { answerText: 'Data storage module', isCorrect: false },
-                    { answerText: 'Function for handling events', isCorrect: false },
-                    { answerText: 'Library for state management', isCorrect: false },
+                    {
+                        answerText: 'The basic building block of an Angular application',
+                        isCorrect: true,
+                    },
+                    { answerText: 'State management module', isCorrect: false },
+                    { answerText: 'Component testing tool', isCorrect: false },
+                    { answerText: 'Function for managing routing', isCorrect: false },
                 ],
             },
             {
-                titleQuestions: 'How to create a state in a class component in React?',
+                titleQuestions:
+                    'What is the name of the decorator for declaring a component in Angular?',
                 answerOption: [
-                    { answerText: 'setState({})', isCorrect: false },
-                    { answerText: 'state = {}', isCorrect: false },
-                    { answerText: 'this.setState({})', isCorrect: false },
-                    { answerText: 'this.state = {}', isCorrect: true },
+                    { answerText: '@Component', isCorrect: true },
+                    { answerText: '@NgModule', isCorrect: false },
+                    { answerText: '@Injectable', isCorrect: false },
+                    { answerText: '@Directive', isCorrect: false },
                 ],
             },
             {
-                titleQuestions: 'How to create a state in a class component in React?',
+                titleQuestions: 'What is a module in Angular?',
                 answerOption: [
-                    { answerText: 'setState({})', isCorrect: false },
-                    { answerText: 'state = {}', isCorrect: false },
-                    { answerText: 'this.setState({})', isCorrect: false },
-                    { answerText: 'this.state = {}', isCorrect: true },
+                    {
+                        answerText:
+                            'A collection of related functionality that can be imported into an application',
+                        isCorrect: true,
+                    },
+                    { answerText: 'Function for state management', isCorrect: false },
+                    { answerText: 'A tool for testing', isCorrect: false },
+                    { answerText: 'A library for styling components', isCorrect: false },
                 ],
             },
             {
-                titleQuestions: 'How to create a state in a class component in React?',
+                titleQuestions:
+                    'Which of the following statements is used to create a reactive thread in Angular?',
                 answerOption: [
-                    { answerText: 'setState({})', isCorrect: false },
-                    { answerText: 'state = {}', isCorrect: false },
-                    { answerText: 'this.setState({})', isCorrect: false },
-                    { answerText: 'this.state = {}', isCorrect: true },
+                    { answerText: 'Observable', isCorrect: true },
+                    { answerText: 'Promise', isCorrect: false },
+                    { answerText: 'Async/Await', isCorrect: false },
+                    { answerText: 'Callback', isCorrect: false },
                 ],
             },
             {
-                titleQuestions: 'How to create a state in a class component in React?',
+                titleQuestions: 'What are directives in Angular?',
                 answerOption: [
-                    { answerText: 'setState({})', isCorrect: false },
-                    { answerText: 'state = {}', isCorrect: false },
-                    { answerText: 'this.setState({})', isCorrect: false },
-                    { answerText: 'this.state = {}', isCorrect: true },
+                    { answerText: 'Instructions for manipulating the DOM', isCorrect: true },
+                    { answerText: 'Functions for state management', isCorrect: false },
+                    { answerText: 'Tools for testing', isCorrect: false },
+                    { answerText: 'Plugins for styling', isCorrect: false },
                 ],
             },
             {
-                titleQuestions: 'How to create a state in a class component in React?',
+                titleQuestions: 'What service is used to work with HTTP requests in Angular?',
                 answerOption: [
-                    { answerText: 'setState({})', isCorrect: false },
-                    { answerText: 'state = {}', isCorrect: false },
-                    { answerText: 'this.setState({})', isCorrect: false },
-                    { answerText: 'this.state = {}', isCorrect: true },
+                    { answerText: 'HttpClient', isCorrect: true },
+                    { answerText: 'HttpService', isCorrect: false },
+                    { answerText: 'HttpModule', isCorrect: false },
+                    { answerText: 'HttpRequest', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'What is Angular Router?',
+                answerOption: [
+                    {
+                        answerText: 'A library for managing in-app routing',
+                        isCorrect: true,
+                    },
+                    { answerText: 'Condition management service', isCorrect: false },
+                    { answerText: 'Component testing tool', isCorrect: false },
+                    { answerText: 'A framework for working with APIs', isCorrect: false },
                 ],
             },
         ],
@@ -306,64 +322,111 @@ export const arrQuiz = [
         title: 'Quiz JavaScript',
         questions: [
             {
-                // id: 'id-1',
-
-                titleQuestions: 'Which company created React.js?',
+                titleQuestions: 'What will the following code output: console.log(typeof null)?',
                 answerOption: [
-                    { answerText: 'A. Google', isCorrect: false },
-                    { answerText: 'B. Facebook', isCorrect: true },
-                    { answerText: 'C. Microsoft', isCorrect: false },
-                    { answerText: 'D. Twitter', isCorrect: false },
+                    { answerText: 'object', isCorrect: true },
+                    { answerText: 'null', isCorrect: false },
+                    { answerText: 'undefined', isCorrect: false },
+                    { answerText: 'number', isCorrect: false },
                 ],
             },
             {
-                // id: 'id-2',
-
-                titleQuestions: 'What is JSX in React?',
+                titleQuestions: 'What method is used to concatenate two or more arrays?',
                 answerOption: [
-                    { answerText: 'JavaScript XML', isCorrect: true },
-                    { answerText: 'JavaScript Extension', isCorrect: false },
-                    { answerText: 'JavaScript eXample', isCorrect: false },
-                    { answerText: 'JavaScript eXperience', isCorrect: false },
+                    { answerText: 'concat', isCorrect: true },
+                    { answerText: 'combine', isCorrect: false },
+                    { answerText: 'merge', isCorrect: false },
+                    { answerText: 'union', isCorrect: false },
                 ],
             },
             {
-                // id: 'id-3',
-
-                titleQuestions: 'Which command is used to create a new React project?',
+                titleQuestions: 'Which comparison operator checks both value and type?',
                 answerOption: [
-                    { answerText: 'npx create-react-app', isCorrect: true },
-                    { answerText: 'npm init react-app', isCorrect: false },
-                    { answerText: 'npm create-react-app', isCorrect: false },
-                    { answerText: 'npx init react-app', isCorrect: false },
+                    { answerText: '==', isCorrect: false },
+                    { answerText: '===', isCorrect: true },
+                    { answerText: '!=', isCorrect: false },
+                    { answerText: '!==', isCorrect: false },
                 ],
             },
             {
-                // id: 'id-4',
-
-                titleQuestions: 'What is a component in React?',
+                titleQuestions: 'How do you create an object in JavaScript?',
                 answerOption: [
-                    { answerText: 'A function or class that returns HTML', isCorrect: true },
-                    { answerText: 'Data storage module', isCorrect: false },
-                    { answerText: 'Function for handling events', isCorrect: false },
-                    { answerText: 'Library for state management', isCorrect: false },
+                    { answerText: 'let obj = {}', isCorrect: true },
+                    { answerText: 'let obj = ()', isCorrect: false },
+                    { answerText: 'let obj = []', isCorrect: false },
+                    { answerText: 'let obj = <>', isCorrect: false },
                 ],
             },
             {
-                // id: 'id-5',
-
-                titleQuestions: 'How to create a state in a class component in React?',
+                titleQuestions: 'What is closure in JavaScript?',
                 answerOption: [
-                    { answerText: 'setState({})', isCorrect: false },
-                    { answerText: 'state = {}', isCorrect: false },
-                    { answerText: 'this.setState({})', isCorrect: false },
-                    { answerText: 'this.state = {}', isCorrect: true },
+                    {
+                        answerText:
+                            'A function that has access to its external function by lexical environment',
+                        isCorrect: true,
+                    },
+                    { answerText: 'A function that calls itself', isCorrect: false },
+                    { answerText: 'A function that executes asynchronously', isCorrect: false },
+                    { answerText: 'A function that has no parameters', isCorrect: false },
                 ],
-                // option1: 'setState({})',
-                // option2: 'state = {}',
-                // option3: 'this.setState({})',
-                // option4: 'this.state = {}',
-                // ans: 4,
+            },
+            {
+                titleQuestions:
+                    'What will the following code output: console.log(0.1 + 0.2 === 0.3)?',
+                answerOption: [
+                    { answerText: 'false', isCorrect: true },
+                    { answerText: 'true', isCorrect: false },
+                    { answerText: 'undefined', isCorrect: false },
+                    { answerText: 'null', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'What is "this" in JavaScript?',
+                answerOption: [
+                    { answerText: 'Function execution context', isCorrect: true },
+                    { answerText: 'A variable with global visibility', isCorrect: false },
+                    { answerText: 'A function that is called automatically', isCorrect: false },
+                    {
+                        answerText: 'An array that contains the function arguments',
+                        isCorrect: false,
+                    },
+                ],
+            },
+            {
+                titleQuestions: 'What are promises in JavaScript?',
+                answerOption: [
+                    {
+                        answerText: 'An object for tracking asynchronous operations',
+                        isCorrect: true,
+                    },
+                    { answerText: 'A function that is executed synchronously', isCorrect: false },
+                    {
+                        answerText: 'An array containing the result of an asynchronous operation',
+                        isCorrect: false,
+                    },
+                    { answerText: 'A method for manipulating the DOM', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'What will the following code output: console.log([] + [])?',
+                answerOption: [
+                    { answerText: '""', isCorrect: true },
+                    { answerText: '[]', isCorrect: false },
+                    { answerText: 'undefined', isCorrect: false },
+                    { answerText: 'null', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'What is async/await in JavaScript?',
+                answerOption: [
+                    { answerText: 'Syntactic sugar for working with promises', isCorrect: true },
+                    { answerText: 'Function for working with arrays', isCorrect: false },
+                    { answerText: 'A method for manipulating the DOM', isCorrect: false },
+                    {
+                        answerText: 'A framework for creating server applications',
+                        isCorrect: false,
+                    },
+                ],
             },
         ],
     },
