dd1f86504dbe815de7224b585693000139db7595
Sergiy Horodetskiy <bryklin.best.1994@gmail.com>
2024-05-26 16:56:19 +0300

    ref-logic-for-Quiz-Quest

--- src/assets/data.js
+++ src/assets/data.js
@@ -4,8 +4,6 @@ export const arrQuiz = [
         title: 'Quiz React',
         questions: [
             {
-                // id: 'id-1',
-
                 titleQuestions: 'Which company created React.js?',
                 answerOption: [
                     { answerText: 'A. Google', isCorrect: false },
@@ -15,8 +13,6 @@ export const arrQuiz = [
                 ],
             },
             {
-                // id: 'id-2',
-
                 titleQuestions: 'What is JSX in React?',
                 answerOption: [
                     { answerText: 'JavaScript XML', isCorrect: true },
@@ -26,8 +22,6 @@ export const arrQuiz = [
                 ],
             },
             {
-                // id: 'id-3',
-
                 titleQuestions: 'Which command is used to create a new React project?',
                 answerOption: [
                     { answerText: 'npx create-react-app', isCorrect: true },
@@ -37,8 +31,6 @@ export const arrQuiz = [
                 ],
             },
             {
-                // id: 'id-4',
-
                 titleQuestions: 'What is a component in React?',
                 answerOption: [
                     { answerText: 'A function or class that returns HTML', isCorrect: true },
@@ -48,8 +40,6 @@ export const arrQuiz = [
                 ],
             },
             {
-                // id: 'id-5',
-
                 titleQuestions: 'How to create a state in a class component in React?',
                 answerOption: [
                     { answerText: 'setState({})', isCorrect: false },
@@ -57,11 +47,54 @@ export const arrQuiz = [
                     { answerText: 'this.setState({})', isCorrect: false },
                     { answerText: 'this.state = {}', isCorrect: true },
                 ],
-                // option1: 'setState({})',
-                // option2: 'state = {}',
-                // option3: 'this.setState({})',
-                // option4: 'this.state = {}',
-                // ans: 4,
+            },
+            {
+                titleQuestions: 'What is React?',
+                answerOption: [
+                    {
+                        answerText: 'JavaScript library for building interfaces',
+                        isCorrect: true,
+                    },
+                    { answerText: 'CSS framework', isCorrect: false },
+                    { answerText: 'HTML preprocessor', isCorrect: false },
+                    { answerText: 'DBMS', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'Which function is used to create components?',
+                answerOption: [
+                    { answerText: 'createComponent', isCorrect: false },
+                    { answerText: 'component', isCorrect: false },
+                    { answerText: 'createElement', isCorrect: true },
+                    { answerText: 'createNode', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'What is state in React?',
+                answerOption: [
+                    { answerText: 'Changeable', isCorrect: false },
+                    { answerText: 'Constant value', isCorrect: false },
+                    { answerText: 'Component state storage', isCorrect: true },
+                    { answerText: 'Class method', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'What are props in React?',
+                answerOption: [
+                    { answerText: 'Function arguments', isCorrect: false },
+                    { answerText: 'External data for the component', isCorrect: true },
+                    { answerText: 'Internal data of the component', isCorrect: false },
+                    { answerText: 'Component methods', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'What is Virtual DOM?',
+                answerOption: [
+                    { answerText: 'A copy of the real DOM', isCorrect: true },
+                    { answerText: 'Synchronization with the real DOM', isCorrect: false },
+                    { answerText: 'Persistent DOM', isCorrect: false },
+                    { answerText: 'Rendering method', isCorrect: false },
+                ],
             },
         ],
     },
@@ -70,64 +103,106 @@ export const arrQuiz = [
         title: 'Quiz Vue',
         questions: [
             {
-                // id: 'id-1',
-
-                titleQuestions: 'Which company created React.js?',
+                titleQuestions: 'What is Vue.js?',
                 answerOption: [
-                    { answerText: 'A. Google', isCorrect: false },
-                    { answerText: 'B. Facebook', isCorrect: true },
-                    { answerText: 'C. Microsoft', isCorrect: false },
-                    { answerText: 'D. Twitter', isCorrect: false },
+                    {
+                        answerText: 'JavaScript library for building interfaces',
+                        isCorrect: true,
+                    },
+                    { answerText: 'CSS framework', isCorrect: false },
+                    { answerText: 'HTML preprocessor', isCorrect: false },
+                    { answerText: 'DBMS', isCorrect: false },
                 ],
             },
             {
-                // id: 'id-2',
-
-                titleQuestions: 'What is JSX in React?',
+                titleQuestions: 'Who created Vue.js?',
                 answerOption: [
-                    { answerText: 'JavaScript XML', isCorrect: true },
-                    { answerText: 'JavaScript Extension', isCorrect: false },
-                    { answerText: 'JavaScript eXample', isCorrect: false },
-                    { answerText: 'JavaScript eXperience', isCorrect: false },
+                    { answerText: 'Evan Yu', isCorrect: true },
+                    { answerText: 'Ryan Dahl', isCorrect: false },
+                    { answerText: 'Jordan Walke', isCorrect: false },
+                    { answerText: 'Thomas Finch', isCorrect: false },
                 ],
             },
             {
-                // id: 'id-3',
-
-                titleQuestions: 'Which command is used to create a new React project?',
+                titleQuestions: 'What is a main object in Vue.js?',
                 answerOption: [
-                    { answerText: 'npx create-react-app', isCorrect: true },
-                    { answerText: 'npm init react-app', isCorrect: false },
-                    { answerText: 'npm create-react-app', isCorrect: false },
-                    { answerText: 'npx init react-app', isCorrect: false },
+                    { answerText: 'Vue instance', isCorrect: true },
+                    { answerText: 'Component', isCorrect: false },
+                    { answerText: 'Directive', isCorrect: false },
+                    { answerText: 'Mixin', isCorrect: false },
                 ],
             },
             {
-                // id: 'id-4',
-
-                titleQuestions: 'What is a component in React?',
+                titleQuestions: 'How are properties declared in Vue components?',
                 answerOption: [
-                    { answerText: 'A function or class that returns HTML', isCorrect: true },
-                    { answerText: 'Data storage module', isCorrect: false },
-                    { answerText: 'Function for handling events', isCorrect: false },
-                    { answerText: 'Library for state management', isCorrect: false },
+                    { answerText: 'data', isCorrect: true },
+                    { answerText: 'state', isCorrect: false },
+                    { answerText: 'properties', isCorrect: false },
+                    { answerText: 'variables', isCorrect: false },
                 ],
             },
             {
-                // id: 'id-5',
-
-                titleQuestions: 'How to create a state in a class component in React?',
+                titleQuestions: 'Which directive is used to display a list of items in Vue?',
                 answerOption: [
-                    { answerText: 'setState({})', isCorrect: false },
-                    { answerText: 'state = {}', isCorrect: false },
-                    { answerText: 'this.setState({})', isCorrect: false },
-                    { answerText: 'this.state = {}', isCorrect: true },
+                    { answerText: 'v-for', isCorrect: true },
+                    { answerText: 'v-if', isCorrect: false },
+                    { answerText: 'v-bind', isCorrect: false },
+                    { answerText: 'v-show', isCorrect: false },
                 ],
-                // option1: 'setState({})',
-                // option2: 'state = {}',
-                // option3: 'this.setState({})',
-                // option4: 'this.state = {}',
-                // ans: 4,
+            },
+            {
+                titleQuestions: 'What is Vue Router?',
+                answerOption: [
+                    {
+                        answerText: 'A library for managing in-app routing',
+                        isCorrect: true,
+                    },
+                    { answerText: 'Condition management service', isCorrect: false },
+                    { answerText: 'Component testing tool', isCorrect: false },
+                    { answerText: 'A framework for working with APIs', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'What method is used to pass data from parent to child in Vue?',
+                answerOption: [
+                    { answerText: 'props', isCorrect: true },
+                    { answerText: 'state', isCorrect: false },
+                    { answerText: 'events', isCorrect: false },
+                    { answerText: 'methods', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'What is Vuex?',
+                answerOption: [
+                    {
+                        answerText: 'A library for state management in Vue applications',
+                        isCorrect: true,
+                    },
+                    { answerText: 'A tool for styling components', isCorrect: false },
+                    { answerText: 'Module for working with forms', isCorrect: false },
+                    { answerText: 'A library for working with server requests', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'How does Vue use two-way data binding?',
+                answerOption: [
+                    { answerText: 'v-model', isCorrect: true },
+                    { answerText: 'v-bind', isCorrect: false },
+                    { answerText: 'v-for', isCorrect: false },
+                    { answerText: 'v-if', isCorrect: false },
+                ],
+            },
+            {
+                titleQuestions: 'What are directives in Vue.js?',
+                answerOption: [
+                    {
+                        answerText: 'Special attributes that add functionality to elements',
+                        isCorrect: true,
+                    },
+                    { answerText: 'Methods for working with the API', isCorrect: false },
+                    { answerText: 'Tools for testing', isCorrect: false },
+                    { answerText: 'Plugins for styling', isCorrect: false },
+                ],
             },
         ],
     },
@@ -180,8 +255,6 @@ export const arrQuiz = [
                 ],
             },
             {
-                // id: 'id-5',
-
                 titleQuestions: 'How to create a state in a class component in React?',
                 answerOption: [
                     { answerText: 'setState({})', isCorrect: false },
@@ -189,11 +262,42 @@ export const arrQuiz = [
                     { answerText: 'this.setState({})', isCorrect: false },
                     { answerText: 'this.state = {}', isCorrect: true },
                 ],
-                // option1: 'setState({})',
-                // option2: 'state = {}',
-                // option3: 'this.setState({})',
-                // option4: 'this.state = {}',
-                // ans: 4,
+            },
+            {
+                titleQuestions: 'How to create a state in a class component in React?',
+                answerOption: [
+                    { answerText: 'setState({})', isCorrect: false },
+                    { answerText: 'state = {}', isCorrect: false },
+                    { answerText: 'this.setState({})', isCorrect: false },
+                    { answerText: 'this.state = {}', isCorrect: true },
+                ],
+            },
+            {
+                titleQuestions: 'How to create a state in a class component in React?',
+                answerOption: [
+                    { answerText: 'setState({})', isCorrect: false },
+                    { answerText: 'state = {}', isCorrect: false },
+                    { answerText: 'this.setState({})', isCorrect: false },
+                    { answerText: 'this.state = {}', isCorrect: true },
+                ],
+            },
+            {
+                titleQuestions: 'How to create a state in a class component in React?',
+                answerOption: [
+                    { answerText: 'setState({})', isCorrect: false },
+                    { answerText: 'state = {}', isCorrect: false },
+                    { answerText: 'this.setState({})', isCorrect: false },
+                    { answerText: 'this.state = {}', isCorrect: true },
+                ],
+            },
+            {
+                titleQuestions: 'How to create a state in a class component in React?',
+                answerOption: [
+                    { answerText: 'setState({})', isCorrect: false },
+                    { answerText: 'state = {}', isCorrect: false },
+                    { answerText: 'this.setState({})', isCorrect: false },
+                    { answerText: 'this.state = {}', isCorrect: true },
+                ],
             },
         ],
     },
--- src/page/QuizQuestions/QuizQuestions.jsx
+++ src/page/QuizQuestions/QuizQuestions.jsx
@@ -1,9 +1,7 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import css from './QuizQuestions.module.css';
-// import { arrQuiz } from '../../assets/data';
 
 function QuizQuestions({ onSelectedQuiz }) {
-    // const [arrQuestions, setArrQuestions] = useState(onSelectedQuiz.questions);
     const arrQuestions = onSelectedQuiz.questions;
 
     let [indexQuestion, setIndexQuestion] = useState(0);
@@ -11,91 +9,28 @@ function QuizQuestions({ onSelectedQuiz }) {
     let [lock, setLock] = useState(false);
     let [score, setScore] = useState(0);
     let [result, setResult] = useState(false);
+    let [answered, setAnswered] = useState(null);
 
-    // let Options1 = useRef(null);
-    // let Options2 = useRef(null);
-    // let Options3 = useRef(null);
-    // let Options4 = useRef(null);
-
-    // let optionsArray = [Options1, Options2, Options3, Options4];
-
-    {
-        /* import { quizQuestions, Question } from './questions'; */
-    }
-
-    {
-        /* const [currentQuestion, setCurrentQuestion] = useState(0); 
-        const [showScore, setShowScore] = useState(false); */
-    }
-
-    const handleAnswerButtonClick = (e, answerOption) => {
-        // console.log(e);
-        // obj = e.target;
+    const handleAnswerButtonClick = (index, answerOption) => {
         if (lock === false) {
-            if (!answerOption.isCorrect) {
-                e.target.classList.add(css.wrong);
-                setLock(true);
-                // const findIndex = arrQuestions.indexOf(answerOption.isCorrect === true);
-                // console.log(findIndex);
-            } else {
+            setAnswered(index);
+            setLock(true);
+            if (answerOption.isCorrect) {
                 setScore(score + 1);
-                e.target.classList.add(css.correct);
-                setLock(true);
-                // answerOption[questions.ans - 1].current.classList.add(css.correct);
             }
-
-            // if (isCorrect) {
-            //     setScore(score + 1);
-            //     e.target.classList.add(css.correct);
-            //     setLock(true);
-            // } else {
-            //     e.target.classList.add(css.wrong);
-            //     setLock(true);
-            //     // answerOption[questions.ans - 1].current.classList.add(css.correct);
-            // }
         }
     };
-
-    // const checkAns = (e, ans) => {
-    //     if (lock === false) {
-    //         if (questions.ans === ans) {
-    //             e.target.classList.add(css.correct);
-    //             setLock(true);
-    //             setScore(prev => prev + 1);
-    //         } else {
-    //             e.target.classList.add(css.wrong);
-    //             setLock(true);
-    //             optionsArray[questions.ans - 1].current.classList.add(css.correct);
-    //         }
-    //     }
-    // };
 
     const next = () => {
         const nextQuestion = indexQuestion + 1;
         if (nextQuestion < arrQuestions.length) {
             setIndexQuestion(nextQuestion);
             setQuestions(arrQuestions[indexQuestion]);
+            setAnswered(null);
+            setLock(false);
         } else {
             setResult(true);
         }
-
-        // if (lock === true) {
-        //     console.log(indexQuestion);
-        //     console.log(arrQuestions.length);
-        //     if (indexQuestion === arrQuestions.length - 1) {
-        //         setResult(true);
-        //         return;
-        //     }
-
-        //     setIndexQuestion(indexQuestion + 1);
-        //     setQuestions(arrQuestions[indexQuestion]);
-        //     setLock(false);
-        //     // optionsArray.map(option => {
-        //     //     // option.current.classList.remove(css.wrong);
-        //     //     // option.current.classList.remove(css.correct);
-        //     //     return null;
-        //     // });
-        // }
     };
 
     const reset = () => {
@@ -103,6 +38,7 @@ function QuizQuestions({ onSelectedQuiz }) {
         setQuestions(arrQuestions[indexQuestion]);
         setScore(0);
         setLock(false);
+        setAnswered(null);
         setResult(false);
     };
 
@@ -129,50 +65,19 @@ function QuizQuestions({ onSelectedQuiz }) {
                             (answerOption, index = Math.random()) => (
                                 <li
                                     key={index}
-                                    className={css.listAnswers}
-                                    onClick={e => handleAnswerButtonClick(e, answerOption)}
+                                    className={`${css.listAnswers} ${
+                                        answered === index
+                                            ? answerOption.isCorrect
+                                                ? css.correct
+                                                : css.wrong
+                                            : ''
+                                    }`}
+                                    onClick={() => handleAnswerButtonClick(index, answerOption)}
                                 >
                                     {answerOption.answerText}
                                 </li>
                             )
                         )}
-
-                        {/* <li
-                            ref={Options1}
-                            className={css.listAnswers}
-                            onClick={e => {
-                                checkAns(e, 1);
-                            }}
-                        >
-                            A. {questions.option1}
-                        </li>
-                        <li
-                            ref={Options2}
-                            className={css.listAnswers}
-                            onClick={e => {
-                                checkAns(e, 2);
-                            }}
-                        >
-                            B. {questions.option2}
-                        </li>
-                        <li
-                            ref={Options3}
-                            className={css.listAnswers}
-                            onClick={e => {
-                                checkAns(e, 3);
-                            }}
-                        >
-                            C. {questions.option3}
-                        </li>
-                        <li
-                            ref={Options4}
-                            className={css.listAnswers}
-                            onClick={e => {
-                                checkAns(e, 4);
-                            }}
-                        >
-                            D. {questions.option4}
-                        </li> */}
                     </ul>
                     <button className={css.button} onClick={next}>
                         Next
